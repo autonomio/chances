fc6de144e1bda62477c9b7fee550d8bbd67479fd
Mikko Kotila <mailme@mikkokotila.com>
2019-01-17 17:42:12 +0200

    fixed importing of matplotlib for mac os x

--- chances/plots.py
+++ chances/plots.py
@@ -1,8 +1,11 @@
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 
-import matplotlib.pyplot as plt
+try:
+    matplotlib
+except NameError:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
 
 
 def oned(data):
